3d44fedf6fd76cbace3bed7149dac20df4230758
dadukhankevin <danieljlosey@gmail.com>
2022-11-12 16:55:22 -0600

    same

--- FinchGA/Layers.py
+++ FinchGA/Layers.py
@@ -536,7 +536,7 @@ class MutateByDirectionalWeights(Layer):
     def native_run(self, data, func):
         these_ones = data.individuals[-self.amount():]
         if self.pool.directional_weights is None:
-            self.pool.directional_weights = np.zeros(len(these_ones[0]))
+            self.pool.directional_weights = np.zeros(len(these_ones[0].genes))
         for i, individual in enumerate(these_ones):
             for n, genes in enumerate(individual.genes):
                 genes += self.pool.directional_weights * self.change_constant()
