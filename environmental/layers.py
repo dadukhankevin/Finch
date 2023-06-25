707af8f6eb1c7789f404c705e9c4c06ed607f247
dadukhankevin <danieljlosey@gmail.com>
2023-06-25 12:05:22 -0500

    asarray

--- environmental/layers.py
+++ environmental/layers.py
@@ -135,7 +135,7 @@ class SortByFitness:
     def run(self, individuals, environment):
         sorted_individuals = sorted(individuals, key=lambda x: -x.fitness)
 
-        return np.asaray(sorted_individuals)
+        return np.asarray(sorted_individuals)
 
 
 class CapPopulation:
