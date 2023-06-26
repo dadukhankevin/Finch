c4436c4b4a1afab1a08b6cbf4ba3558ee73f37fa
dadukhankevin <danieljlosey@gmail.com>
2023-06-26 05:09:54 -0500

    __class__

--- environmental/environments.py
+++ environmental/environments.py
@@ -22,15 +22,16 @@ class Sequential:
             self.iteration = i
             if verbose_every and i % verbose_every == 0:
                 print(f"Generation {i + 1}/{generations}. Max fitness: {fitness}. Population: {len(self.individuals)}")
+            print("i", self.individuals.__class__)
             for layer in self.layers:
                 self.individuals = layer.run(self.individuals, self)
-                self.individuals = list(self.individuals)
             if callback:
                 callback(self.individuals, self)
             if len(self.individuals) == 0:
                 raise NoIndividualsAtEndOfRun("Your environment has a population of 0 after running.")
             if self.original is None:
                 self.original = self.individuals[0]
+            print("o", self.original.__class__)
             if track_float_diff_every and i % track_float_diff_every == 0:
                 self.diff = -(self.original.genes - self.individuals[0].genes)
             fitness = self.individuals[-1].fitness
