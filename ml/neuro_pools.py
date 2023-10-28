0bd2162ab46c8950580bc5d2a6d2fac44d2638b0
dadukhankevin <danieljlosey@gmail.com>
2023-10-28 16:29:41 -0500

    np vs cp

--- ml/neuro_pools.py
+++ ml/neuro_pools.py
@@ -4,7 +4,7 @@ from Finch.genetics.population import Individual
 from Finch.genetics.population import NPCP as np
 from Finch.ml.llm import LLM
 from keras.layers import Layer
-
+import numpy
 from keras.models import Model
 
 def to_numpy_generic(arr):
@@ -71,7 +71,7 @@ class KerasPool:
             elif isinstance(layer, Layer):
                 layer_weights = layer.get_weights()
                 new_weights = [np.random.randn(*w.shape) for w in layer_weights]
-                layer.set_weights(to_numpy_generic(new_weights))
+                layer.set_weights(numpy.asarray(new_weights))
                 flat_weights.extend([w.flatten() for w in new_weights])
                 if flat_weights:
                     total_weights += len(flat_weights[-1])
